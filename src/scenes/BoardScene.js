4359d66948d54f533c93a68662169ab7e75f654f
artemb <a.bobrovskij@gmail.com>
2020-04-25 09:45:38 +0100

    Added zombie to the grid

--- src/scenes/BoardScene.js
+++ src/scenes/BoardScene.js
@@ -1,5 +1,23 @@
 import Phaser from 'phaser'
 
+const GRID_WIDTH = 24
+const GRID_HEIGHT = 24
+const SCREEN_WIDTH = 1400
+const SCREEN_HEIGHT = 752
+const BOARD_WIDTH = 903
+const BOARD_HEIGHT = SCREEN_HEIGHT
+const CELL_WIDTH = BOARD_WIDTH / GRID_WIDTH
+const CELL_HEIGHT = BOARD_HEIGHT / GRID_HEIGHT
+
+function place_on_grid(grid_x, grid_y, axis) {
+    const BOARD_X_OFFSET = (SCREEN_WIDTH - BOARD_WIDTH) / 2
+    if (axis == 'x') {
+        return BOARD_X_OFFSET + CELL_WIDTH * grid_x - CELL_WIDTH / 2
+    } else {
+        return CELL_HEIGHT * grid_y - CELL_HEIGHT / 2
+    }
+}
+
 export default class BoardScene extends Phaser.Scene {
     constructor() {
         super('game-scene')
@@ -7,9 +25,11 @@ export default class BoardScene extends Phaser.Scene {
 
     preload () {
         this.load.image('board', 'assets/board.png')
+        this.load.image('zombie', 'assets/characters/zombies/male/idle (1).png', )
     }
 
     create () {
-        this.add.image(700, 376, 'board')
+        this.add.image(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 'board')
+        this.add.image(place_on_grid(1, 1, 'x'), place_on_grid(1, 1, 'y'), 'zombie').setScale(0.08)
     }
 }
