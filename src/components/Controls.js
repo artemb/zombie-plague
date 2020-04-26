8887a1f964bc0c990ede65094246d1eb09cba22c
artemb <a.bobrovskij@gmail.com>
2020-04-26 10:27:18 +0100

    Controls component

---
+++ src/components/Controls.js
@@ -0,0 +1,44 @@
+import Phaser from 'phaser'
+import Board from './Board.js'
+
+export default class Controls extends Phaser.GameObjects.Container {
+    constructor(scene, x, y, zombie) {
+        super(scene, x, y)
+        this.setSize(scene.game.config.width - Board.BOARD_WIDTH, scene.game.config.height)
+        console.log(scene.game.config.width - Board.BOARD_WIDTH, scene.game.config.height)
+
+        scene.add.existing(this);
+
+        this.zombie = zombie
+
+        // let gr = scene.add.graphics({ fillStyle: { color: 0x0000aa } });
+        // this.add(gr)
+        // gr.fillRectShape(new Phaser.Geom.Rectangle(0, 0, Math.max(50, this.width), Math.max(70, this.height)))
+
+        function move_callback(x, y) {
+            return function (pointer) {
+                this.setTint(0xFF0000)
+                zombie.move_forward();
+            }
+        }
+
+        function btn_up(pointer) {
+            this.clearTint()
+        }
+
+        let left_btn = this.create_button(50, 50, 1, move_callback(-1, 0), btn_up)
+        let right_btn = this.create_button(120, 50, 4, move_callback(1, 0), btn_up)
+        let fwd_btn = this.create_button(50, 120, 2, move_callback(0, -1), btn_up)
+        let bwd_btn = this.create_button(120, 120, 3, move_callback(0, 1), btn_up)
+    }
+
+    create_button(x, y, frame, onPointerDown, onPointerUp) {
+        let btn = this.scene.add.sprite(x, y, 'buttons', frame).setScale(0.3).setInteractive();
+        btn.on('pointerdown', onPointerDown)
+        btn.on('pointerup', onPointerUp)
+        btn.on('pointerout', onPointerUp)
+        this.add(btn)
+
+        return btn
+    }    
+}
--- src/scenes/BoardScene.js
+++ src/scenes/BoardScene.js
@@ -1,6 +1,7 @@
 import Phaser, { Tilemaps } from 'phaser'
 import Board from '../components/Board.js'
 import Character from '../components/Character.js'
+import Controls from '../components/Controls.js'
 
 export default class BoardScene extends Phaser.Scene {
     constructor() {
@@ -13,13 +14,7 @@ export default class BoardScene extends Phaser.Scene {
         this.load.spritesheet('buttons', 'assets/ui/controls.png', {frameWidth: 200, frameHeight: 215})
     }
 
-    create_button(x, y, frame, onPointerDown, onPointerUp) {
-        let btn = this.add.sprite(x, y, 'buttons', frame).setScale(0.3).setInteractive();
-        btn.on('pointerdown', onPointerDown)
-        btn.on('pointerup', onPointerUp)
-        btn.on('pointerout', onPointerUp)
-        return btn
-    }
+
 
     create () {
         this.board = new Board(this, this.game.config.width - Board.BOARD_WIDTH / 2, this.game.config.height / 2)
@@ -27,21 +22,7 @@ export default class BoardScene extends Phaser.Scene {
         let zombie = new Character(this, this.board, 'zombie')
         zombie.position(3, 3)
 
-        function move_callback(x, y) {
-            return function (pointer) {
-                this.setTint(0xFF0000)
-                zombie.x += x * CELL_WIDTH;
-                zombie.y += y * CELL_WIDTH;
-            }
-        }
+        this.controls = new Controls(this, 0, 0, zombie)
 
-        function btn_up(pointer) {
-            this.clearTint()
-        }
-
-        // let left_btn = this.create_button(50, 50, 1, move_callback(-1, 0), btn_up)
-        // let right_btn = this.create_button(120, 50, 4, move_callback(1, 0), btn_up)
-        // let fwd_btn = this.create_button(50, 120, 2, move_callback(0, -1), btn_up)
-        // let bwd_btn = this.create_button(120, 120, 3, move_callback(0, 1), btn_up)
     }
 }
