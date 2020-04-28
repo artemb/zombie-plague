51e8eb681aebd4f42162e8c0f3d541d18aab0400
artemb <a.bobrovskij@gmail.com>
2020-04-28 20:16:14 +0100

    Basic sockets working

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__, static_folder='dist/')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+if __name__ == '__main__':
+    socketio.run(app)
