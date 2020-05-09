b92fd595ba4f1b4984abc90958b2a5288cb62e6e
artemb <a.bobrovskij@gmail.com>
2020-05-09 09:58:57 +0100

    Remaining AP label

--- client/src/components/Controls.ts
+++ client/src/components/Controls.ts
@@ -14,6 +14,8 @@ export default class Controls extends Phaser.GameObjects.Container {
   zombie: Character;
   private stateManager: StateManager;
   private btn_group: Phaser.GameObjects.Group;
+  private turnLabel: Phaser.GameObjects.Text;
+  private remainingAPLabel: Phaser.GameObjects.Text;
 
   static preload(scene:Phaser.Scene, prefix: string) {
     scene.load.html('nameform', prefix + 'assets/username.html')
@@ -31,27 +33,42 @@ export default class Controls extends Phaser.GameObjects.Container {
     this.stateManager = stateManager;
     this.stateManager.on('gamestatechange', () => this.onGameStateUpdate());
 
-    this.drawControls(character)
+    this.drawControls(character);
+    this.drawText(character);
+  }
+
+  drawText(character) {
+    this.turnLabel = this.scene.add.text(this.width / 2, 50, 'Your turn', {
+      'color': '#FFF',
+      'fontSize': '18px',
+    }).setOrigin(0.5, 0.5);
+    this.add(this.turnLabel);
+
+    this.remainingAPLabel = this.scene.add.text(this.width / 2, 400, 'Remaining AP: 0', {
+      'color': '#FFF',
+      'fontSize': '16px',
+    }).setOrigin(0.5, 0.5);
+    this.add(this.remainingAPLabel);
   }
 
   drawControls(character) {
     this.btn_group = this.scene.add.group();
-    let btn = this.create_button(50, 50, 1, () =>
+    let btn = this.create_button(50, 150, 1, () =>
         this.sendUpdate(character, Action.LEFT)
     );
     this.btn_group.add(btn);
 
-    btn = this.create_button(120, 50, 4, () =>
+    btn = this.create_button(120, 150, 4, () =>
         this.sendUpdate(character, Action.RIGHT)
     );
     this.btn_group.add(btn);
 
-    btn = this.create_button(50, 120, 2, () =>
+    btn = this.create_button(50, 220, 2, () =>
         this.sendUpdate(character, Action.FORWARD)
     );
     this.btn_group.add(btn);
 
-    btn = this.create_button(120, 120, 3, () =>
+    btn = this.create_button(120, 220, 3, () =>
         this.sendUpdate(character, Action.BACKWARD)
     );
     this.btn_group.add(btn);
@@ -75,7 +92,11 @@ export default class Controls extends Phaser.GameObjects.Container {
   onGameStateUpdate() {
     this.btn_group.setVisible(false);
     if (this.stateManager.isOurTurn()) {
+      this.turnLabel.setText('Your turn');
       this.btn_group.setVisible(true);
+    } else {
+      this.turnLabel.setText('Someone\'s turn');
     }
+    this.remainingAPLabel.setText(`Remaining AP: ${this.stateManager.turn['remainingAP']}`)
   }
 }
